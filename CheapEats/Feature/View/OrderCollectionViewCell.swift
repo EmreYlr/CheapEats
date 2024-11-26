0feea05982dc34fd251f70960e562e21a371b06e
Emre Yeler <yeleremre@hotmail.com>
2024-11-26 23:16:52 +0300

    Horizontal scroll added

---
+++ CheapEats/Feature/View/OrderCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  OrderCollectionViewCell.swift
+//  CheapEats
+//
+//  Created by Emre on 26.11.2024.
+//
+
+import UIKit
+
+class OrderCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
